99159d433947cec44fd09736257ed4b306c36d20
M.S Édit <mairy.gillen@gmail.com>
2026-06-26 12:42:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from datetime import datetime
 
 app = Flask(__name__)
 app.secret_key = "domaine_lea_demo_2026"
+PRIX = {
+    "basse": 120,
+    "moyenne": 150,
+    "haute": 190
+}
 
 
 @app.route("/")
@@ -24,8 +30,62 @@ def tarifs():
     return render_template("tarifs.html")
 
 
-@app.route("/reservation")
+from datetime import datetime
+
+@app.route("/reservation", methods=["GET", "POST"])
 def reservation():
+
+    if request.method == "POST":
+
+        arrivee = request.form["arrivee"]
+        depart = request.form["depart"]
+        adultes = int(request.form["adultes"])
+        enfants = int(request.form["enfants"])
+        nom = request.form["nom"]
+        prenom = request.form["prenom"]
+        telephone = request.form["telephone"]
+        email = request.form["email"]
+        message = request.form["message"]
+
+        date_arrivee = datetime.strptime(arrivee, "%Y-%m-%d")
+        date_depart = datetime.strptime(depart, "%Y-%m-%d")
+
+        nuits = (date_depart - date_arrivee).days
+
+        mois = date_arrivee.month
+
+        if mois in [7, 8]:
+            prix_nuit = PRIX["haute"]
+            saison = "Haute saison"
+
+        elif mois in [5, 6, 9]:
+            prix_nuit = PRIX["moyenne"]
+            saison = "Moyenne saison"
+
+        else:
+            prix_nuit = PRIX["basse"]
+            saison = "Basse saison"
+
+        total = nuits * prix_nuit
+
+        session["reservation"] = {
+            "arrivee": arrivee,
+            "depart": depart,
+            "adultes": adultes,
+            "enfants": enfants,
+            "nom": nom,
+            "prenom": prenom,
+            "telephone": telephone,
+            "email": email,
+            "message": message,
+            "nuits": nuits,
+            "prix_nuit": prix_nuit,
+            "saison": saison,
+            "total": total
+        }
+
+        return redirect("/panier")
+
     return render_template("reservation.html")
 
 
